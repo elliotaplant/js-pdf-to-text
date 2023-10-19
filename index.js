600b366a9f3873d5d5e999d9b298fde75120f124
Elliot Plant <elliotaplant@gmail.com>
2023-10-19 09:14:40 -0700

    With file upload

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
 function loadPdf() {
-    const fileInput = document.getElementById('pdf-file');
-    const file = fileInput.files[0];
+  const fileInput = document.getElementById('pdf-file');
+  const file = fileInput.files[0];
 
-    if (file) {
-        const fileReader = new FileReader();
-        fileReader.onload = function (event) {
-            const typedArray = new Uint8Array(event.target.result);
+  if (file) {
+    const fileReader = new FileReader();
+    fileReader.onload = function (event) {
+      const typedArray = new Uint8Array(event.target.result);
+      document.getElementById('pdf-text-container').style.display = 'block';
 
-            const loadingTask = pdfjsLib.getDocument({ data: typedArray });
-            loadingTask.promise.then(pdf => {
-                const { numPages } = pdf;
-                let currentPage = 1;
+      const loadingTask = pdfjsLib.getDocument({ data: typedArray });
+      loadingTask.promise.then((pdf) => {
+        const { numPages } = pdf;
+        let currentPage = 1;
 
-                function getNextPage() {
-                    pdf.getPage(currentPage).then(page => {
+        function getNextPage() {
+          pdf.getPage(currentPage).then((page) => {
+            const charSize = 3.7; // number of width units per character
 
-                        const charSize = 3.7; // number of width units per character
+            page.getTextContent().then((text) => {
+              let lastY,
+                textLine = '';
 
-                        page.getTextContent().then(text => {
-                            let lastY, textLine = '';
+              text.items.forEach((item) => {
+                const { transform, str } = item;
+                const x = transform[4];
+                const y = transform[5];
 
-                            text.items.forEach(item => {
-                                const { transform, str } = item;
-                                const x = transform[4];
-                                const y = transform[5];
+                if (lastY === undefined) lastY = y;
 
-                                if (lastY === undefined) lastY = y;
-
-                                // Newline if new Y is found
-                                if (Math.abs(lastY - y) > 3) {
-                                    document.getElementById('pdf-text-content').innerText += textLine + '\n';
-                                    textLine = '';
-                                    lastY = y;
-                                }
-
-                                // Adding spaces based on X distance
-                                const spacesToAdd = Math.round((x - (textLine.length * charSize)) / charSize);
-                                if (spacesToAdd > 0) textLine += ' '.repeat(spacesToAdd);
-
-                                textLine += str;
-                            });
-
-                            document.getElementById('pdf-text-content').innerText += textLine + '\n';
-
-                            if (currentPage < numPages) {
-                                currentPage++;
-                                getNextPage();
-                            }
-                        });
-                    });
+                // Newline if new Y is found
+                if (Math.abs(lastY - y) > 3) {
+                  document.getElementById('pdf-text-content').innerText += textLine + '\n';
+                  textLine = '';
+                  lastY = y;
                 }
 
-                getNextPage();
-            });
-        };
+                // Adding spaces based on X distance
+                const spacesToAdd = Math.round((x - textLine.length * charSize) / charSize);
+                if (spacesToAdd > 0) textLine += ' '.repeat(spacesToAdd);
 
-        fileReader.readAsArrayBuffer(file);
-    } else {
-        alert('Please select a PDF file.');
-    }
+                textLine += str;
+              });
+
+              document.getElementById('pdf-text-content').innerText += textLine + '\n';
+
+              if (currentPage < numPages) {
+                currentPage++;
+                getNextPage();
+              }
+            });
+          });
+        }
+
+        getNextPage();
+      });
+    };
+
+    fileReader.readAsArrayBuffer(file);
+  } else {
+    alert('Please select a PDF file.');
+  }
 }
